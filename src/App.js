865f4f7844a611770a54b23eff0e803ecd6688ff
subham jalan <subhamj13@gmail.com>
2024-07-10 00:00:16 +0530

    changes

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [count,setCount] = useState(0);
+  const incrementDecrementHandler = (event)=>{
+    event.target.innerHTML === "Increment" ? setCount(preval => ++preval) : setCount(preval => --preval);
+  }
+  return (
+    <div className="App">
+        <h1>Counter App</h1>
+        <p>count: {count}</p>
+        <div>
+          <button onClick={incrementDecrementHandler}>Increment</button>
+          <button onClick={incrementDecrementHandler}>Decrement</button>
+        </div>
+    </div>
+  );
+}
+
+export default App;
